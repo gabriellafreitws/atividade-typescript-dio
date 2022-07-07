24273d3444e6a150bff83fee5ae481390782fdfa
Gabriella Freitas ✦ <105538076+gabriellafreitws@users.noreply.github.com>
2022-07-07 17:33:28 -0300

    Update and rename desafio-3 to desafio-3.ts

--- desafio-3.ts
+++ desafio-3.ts
@@ -24,7 +24,7 @@ botaoLimpar.addEventListener('click', function () {
 });
 */
 
-Minha resposta:
+//Minha resposta:
 
 let botaoAtualizar = document.getElementById('atualizar-saldo') as HTMLButtonElement;
 let botaoLimpar = document.getElementById('limpar-saldo') as HTMLButtonElement;
